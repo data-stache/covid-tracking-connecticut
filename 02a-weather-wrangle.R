d4a663488580a21c642f7551e051d15bda5da9e4
Andrew F. Griffin <andrewfgriffin@gmail.com>
2020-10-28 12:42:01 -0400

    Weather Debug / Data Updates

--- 02a-weather-wrangle.R
+++ 02a-weather-wrangle.R
@@ -2,99 +2,28 @@ library(tidyverse)
 library(lubridate)
 library(rnoaa)
 library(zoo)
+load("rda/ct_stations.rda")
 
-weather <- read.csv("data/ct_weather.csv")
-stations <- weather %>%
-  filter(!is.na(TAVG)) %>%
-  .$STATION
-stations <- unique(stations)
+ct_weather <- meteo_pull_monitors(ct_stations, date_min = "2020-03-01", var = c("TMIN", "TMAX"))
 
-##### WEATHER STATION 1 CONNECTICUT #####
-# PULL DATA FOR STATION
-ct_weather_1 <- ghcnd(stations[1]) %>%
-  filter(element == "TAVG")
-# COLUMN NAME PULL
-test <- names(ct_weather_1)
-col_ind <- test %>%
-  str_detect(.,"VALUE")
-col_ind[1:3] <- TRUE
-# COLUMN REPLACE
-ct_weather_1 <- ct_weather_1[,col_ind] %>%
-  # ONLY 2020
-  filter(year == 2020) %>%
-  # GATHER TABLE TO TIDY TABLE
-  gather(day, avg_t, VALUE1:VALUE31) %>%
-  # ARRANGE BY MONTH
-  arrange(month) 
-
-# STRIP "VALUE"
-ct_weather_1$day <- str_replace(ct_weather_1$day, "VALUE", "")
-
-
-ct_weather_1 <- ct_weather_1 %>%
-  # DAY TO INTERGER
-  mutate(day = as.integer(day),
-         # CONVERT TO FARENHEIGHT
-         avg_t = round(((avg_t * .1) * (9/5)) + 32),
-         # ADD LEADING 0's
-         month = str_pad(month, 2, pad = "0"),
-         day = str_pad(day, 2, pad = "0"),
-         # PASTE DATE
-         date = paste(year, month, day, sep = "-")) %>%
-  # FILTERS DAYS THAT DON'T EXIST
-  filter(!date %in% c("2020-02-30", "2020-02-31", "2020-04-31", "2020-06-31", "2020-09-31", "2020-11-31")) %>%
-  # CONVERTS TO DATE
-  mutate(date = ymd(date))
-
-# GRABS COLUMNS
-ct_weather_1 <- ct_weather_1[,c(6,5)]
-
-##### WEATHER STATION 2 #####
-# PULL DATA FOR STATION
-ct_weather_2 <- ghcnd(stations[1]) %>%
-  filter(element == "TAVG")
-# COLUMN NAME PULL
-test <- names(ct_weather_2)
-col_ind <- test %>%
-  str_detect(.,"VALUE")
-col_ind[1:3] <- TRUE
-# COLUMN REPLACE
-ct_weather_2 <- ct_weather_2[,col_ind] %>%
-  # ONLY 2020
-  filter(year == 2020) %>%
-  # GATHER TABLE TO TIDY TABLE
-  gather(day, avg_t, VALUE1:VALUE31) %>%
-  # ARRANGE BY MONTH
-  arrange(month) 
-
-# STRIP "VALUE"
-ct_weather_2$day <- str_replace(ct_weather_2$day, "VALUE", "")
-
-
-ct_weather_2 <- ct_weather_2 %>%
-  # DAY TO INTERGER
-  mutate(day = as.integer(day),
-         # CONVERT TO FARENHEIGHT
-         avg_t = round(((avg_t * .1) * (9/5)) + 32),
-         # ADD LEADING 0's
-         month = str_pad(month, 2, pad = "0"),
-         day = str_pad(day, 2, pad = "0"),
-         # PASTE DATE
-         date = paste(year, month, day, sep = "-")) %>%
-  # FILTERS DAYS THAT DON'T EXIST
-  filter(!date %in% c("2020-02-30", "2020-02-31", "2020-04-31", "2020-06-31", "2020-09-31", "2020-11-31")) %>%
-  # CONVERTS TO DATE
-  mutate(date = ymd(date))
-
-# GRABS COLUMNS
-ct_weather_2 <- ct_weather_2[,c(6,5)]
-
-ct_weather <- rbind(ct_weather_1, ct_weather_2) %>%
+ct_weather <- ct_weather %>%
   arrange(date) %>%
+  # CONVERT TO °F
+  mutate(tmax = ((tmax * .1) * (9/5)) + 32,
+         tmin = ((tmin * .1) * (9/5)) + 32,
+         # ADD AVERAGE TEMPERATURE
+         tavg = (tmin + tmax) / 2) %>%
   group_by(date) %>%
-  summarise(avg_t = mean(avg_t, na.rm = TRUE)) %>%
-  filter(!is.na(avg_t)) %>%
-  mutate(avg_t_07da = rollapply(avg_t, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right")) %>%
-  ungroup() 
+  # AVERAGE ALL CT STATIONS
+  summarise(tavg = mean(tavg, na.rm = TRUE),
+            tmax = mean(tmax, na.rm = TRUE),
+            tmin = mean(tmin, na.rm = TRUE)) %>%
+  # 7 DAY ROLLING AVERAGE
+  mutate(tavg_07da = rollapply(tavg, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
+         tmax_07da = rollapply(tmax, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
+         tmin_07da = rollapply(tmin, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right")) %>%
+  ungroup()
+# REORDER COLUMNS
+ct_weather <- ct_weather[,c(1,2,5,3,6,4,7)]
 save(ct_weather, file = "rda/ct_weather.rda")
 
--- 21-weather-factor.R
+++ 21-weather-factor.R
@@ -1,7 +1,8 @@
 library(broom)
 library(tidyverse)
+load("rda/theme_DataStache.rda")
 load("rda/covid_ct.rda")
-load("rda/weather.rda")
+load("rda/ct_weather.rda")
 
 dat <- covid_ct %>%
   inner_join(ct_weather)
@@ -9,27 +10,54 @@ dat <- covid_ct %>%
 ##### SINCE APRIL PANDEMIC #####
 P1 <- dat %>%
   filter(date >= ymd(20200401)) %>%
-  ggplot(aes(x = avg_t_07da, y = new_cases_07da)) +
+  ggplot(aes(x = tavg_07da, y = new_cases_07da)) +
   geom_point(size = 1, alpha = .5) +
   geom_smooth()
 P1
 
 dat %>%
-  ggplot(aes(x = avg_t_07da, y = new_cases_07da)) +
+  ggplot(aes(x = tavg_07da, y = new_cases_07da)) +
   geom_point(size = 1, alpha = .5) +
   facet_wrap(. ~ month(date))
 
 model <- dat %>%
   filter(date >= ymd(20200401)) %>%
-  lm(new_cases_07da ~ avg_t_07da, data = .)
+  lm(new_cases_07da ~ tavg_07da, data = .)
 model
 
 coefs <- tidy(model, conf.int = TRUE)
 coefs
 
-new_temps <- data.frame(avg_t_07da = seq(20,60,5))
 
-predict(model, newdata = new_temps)
+##### PREDICTED CASES PER DAY #####
+new_temps <- data.frame(tavg_07da = seq(0,60,5))
+new_temps <- predict(model, newdata = new_temps) %>%
+  cbind(new_temps) %>%
+  rename(new_cases_07da_mod = ".")
+new_temps <- new_temps[,c(2,1)]
+new_temps
 
 
+##### PLOT PREDICTION VS ACTUAL #####
+dat %>%
+  filter(date >= ymd(20200401)) %>%
+  ggplot(aes(x = tavg_07da, y = new_cases_07da)) +
+  geom_hline(yintercept = 0, col = "grey60", size = .7) +
+  geom_point(size = .5, alpha = .3, col = "dark blue") +
+  geom_smooth(method = "lm") +
+  scale_x_continuous(breaks = seq(0,100,5)) +
+  scale_y_continuous(breaks = seq(0,2000,200), expand = c(0,0)) +
+  coord_cartesian(ylim = c(0,max(dat$new_cases_07da, na.rm = TRUE) * 1.05)) +
+  ggtitle("Does Average Temperature Affect Covid-19 Infection Rates") +
+  xlab("7 Day Average Temperature (°F)") +
+  ylab("7 Day Average Daily New Cases") +
+  theme_DataStache() +
+  theme(axis.title.x.bottom = element_text(size = rel(.7), margin = margin(5, 1, 1, 1)),
+        axis.title.y.left = element_text(angle = 90, size = rel(.6), margin = margin(1, 7, 1, 1)))
 
+p_width <- 9
+p_height <- (9/16) * p_width
+ggsave("figs/ct-weather-infection-regression.png",
+       width = p_width,
+       height = p_height,
+       dpi = "retina")
