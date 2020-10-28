e4173d8de00e36fa79cd02451b3c2ad76612eb20
Andrew F. Griffin <andrewfgriffin@gmail.com>
2020-10-28 00:06:59 -0400

    Weather Restructure

--- 02a-weather-wrangle.R
+++ 02a-weather-wrangle.R
@@ -1,24 +1,100 @@
 library(tidyverse)
+library(lubridate)
+library(rnoaa)
+library(zoo)
 
-##### WRANGLE MOBILITY COVID DATA ####
 weather <- read.csv("data/ct_weather.csv")
+stations <- weather %>%
+  filter(!is.na(TAVG)) %>%
+  .$STATION
+stations <- unique(stations)
 
-names(weather)
+##### WEATHER STATION 1 CONNECTICUT #####
+# PULL DATA FOR STATION
+ct_weather_1 <- ghcnd(stations[1]) %>%
+  filter(element == "TAVG")
+# COLUMN NAME PULL
+test <- names(ct_weather_1)
+col_ind <- test %>%
+  str_detect(.,"VALUE")
+col_ind[1:3] <- TRUE
+# COLUMN REPLACE
+ct_weather_1 <- ct_weather_1[,col_ind] %>%
+  # ONLY 2020
+  filter(year == 2020) %>%
+  # GATHER TABLE TO TIDY TABLE
+  gather(day, avg_t, VALUE1:VALUE31) %>%
+  # ARRANGE BY MONTH
+  arrange(month) 
 
-ct_weather <- weather %>%
-  rename(station = STATION,
-         name = NAME,
-         date = DATE,
-         avg_t = TAVG,
-         hi = TMAX,
-         lo = TMIN) %>%
-  filter(!is.na(avg_t) & !is.na(hi) & !is.na(lo)) %>%
-  mutate(date = ymd(date)) %>%
+# STRIP "VALUE"
+ct_weather_1$day <- str_replace(ct_weather_1$day, "VALUE", "")
+
+
+ct_weather_1 <- ct_weather_1 %>%
+  # DAY TO INTERGER
+  mutate(day = as.integer(day),
+         # CONVERT TO FARENHEIGHT
+         avg_t = round(((avg_t * .1) * (9/5)) + 32),
+         # ADD LEADING 0's
+         month = str_pad(month, 2, pad = "0"),
+         day = str_pad(day, 2, pad = "0"),
+         # PASTE DATE
+         date = paste(year, month, day, sep = "-")) %>%
+  # FILTERS DAYS THAT DON'T EXIST
+  filter(!date %in% c("2020-02-30", "2020-02-31", "2020-04-31", "2020-06-31", "2020-09-31", "2020-11-31")) %>%
+  # CONVERTS TO DATE
+  mutate(date = ymd(date))
+
+# GRABS COLUMNS
+ct_weather_1 <- ct_weather_1[,c(6,5)]
+
+##### WEATHER STATION 2 #####
+# PULL DATA FOR STATION
+ct_weather_2 <- ghcnd(stations[1]) %>%
+  filter(element == "TAVG")
+# COLUMN NAME PULL
+test <- names(ct_weather_2)
+col_ind <- test %>%
+  str_detect(.,"VALUE")
+col_ind[1:3] <- TRUE
+# COLUMN REPLACE
+ct_weather_2 <- ct_weather_2[,col_ind] %>%
+  # ONLY 2020
+  filter(year == 2020) %>%
+  # GATHER TABLE TO TIDY TABLE
+  gather(day, avg_t, VALUE1:VALUE31) %>%
+  # ARRANGE BY MONTH
+  arrange(month) 
+
+# STRIP "VALUE"
+ct_weather_2$day <- str_replace(ct_weather_2$day, "VALUE", "")
+
+
+ct_weather_2 <- ct_weather_2 %>%
+  # DAY TO INTERGER
+  mutate(day = as.integer(day),
+         # CONVERT TO FARENHEIGHT
+         avg_t = round(((avg_t * .1) * (9/5)) + 32),
+         # ADD LEADING 0's
+         month = str_pad(month, 2, pad = "0"),
+         day = str_pad(day, 2, pad = "0"),
+         # PASTE DATE
+         date = paste(year, month, day, sep = "-")) %>%
+  # FILTERS DAYS THAT DON'T EXIST
+  filter(!date %in% c("2020-02-30", "2020-02-31", "2020-04-31", "2020-06-31", "2020-09-31", "2020-11-31")) %>%
+  # CONVERTS TO DATE
+  mutate(date = ymd(date))
+
+# GRABS COLUMNS
+ct_weather_2 <- ct_weather_2[,c(6,5)]
+
+ct_weather <- rbind(ct_weather_1, ct_weather_2) %>%
+  arrange(date) %>%
   group_by(date) %>%
-  summarise(avg_T = mean(avg_t),
-            avg_HI = mean(hi),
-            avg_LO = mean(lo)) %>%
-  mutate(avg_T_07da = rollapply(avg_T, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
-         avg_HI_07da = rollapply(avg_HI, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"),
-         avg_LO_07da = rollapply(avg_LO, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"))
-save(weather, file = "rda/weather.rda")
+  summarise(avg_t = mean(avg_t, na.rm = TRUE)) %>%
+  filter(!is.na(avg_t)) %>%
+  mutate(avg_t_07da = rollapply(avg_t, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right")) %>%
+  ungroup() 
+save(ct_weather, file = "rda/ct_weather.rda")
+
--- 21-weather-factor.R
+++ 21-weather-factor.R
@@ -9,25 +9,25 @@ dat <- covid_ct %>%
 ##### SINCE APRIL PANDEMIC #####
 P1 <- dat %>%
   filter(date >= ymd(20200401)) %>%
-  ggplot(aes(x = avg_T_07da, y = new_cases_07da)) +
+  ggplot(aes(x = avg_t_07da, y = new_cases_07da)) +
   geom_point(size = 1, alpha = .5) +
   geom_smooth()
 P1
 
 dat %>%
-  ggplot(aes(x = avg_T_07da, y = new_cases_07da)) +
+  ggplot(aes(x = avg_t_07da, y = new_cases_07da)) +
   geom_point(size = 1, alpha = .5) +
   facet_wrap(. ~ month(date))
 
 model <- dat %>%
   filter(date >= ymd(20200401)) %>%
-  lm(new_cases_07da ~ avg_T_07da, data = .)
+  lm(new_cases_07da ~ avg_t_07da, data = .)
 model
 
 coefs <- tidy(model, conf.int = TRUE)
 coefs
 
-new_temps <- data.frame(avg_T_07da = seq(20,60,5))
+new_temps <- data.frame(avg_t_07da = seq(20,60,5))
 
 predict(model, newdata = new_temps)
 
