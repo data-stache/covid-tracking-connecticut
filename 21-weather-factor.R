cb14217008bdaf8529833dbca82b74fe87184168
Andrew F. Griffin <andrewfgriffin@gmail.com>
2020-11-10 20:27:09 -0500

    2020 11 10 catchup day

--- 21-weather-factor.R
+++ 21-weather-factor.R
@@ -1,11 +1,82 @@
 library(broom)
 library(tidyverse)
+library(caret)
+
 load("rda/theme_DataStache.rda")
 load("rda/covid_ct.rda")
 load("rda/ct_weather.rda")
 
+head(covid_ct)
+head(weather_usa)
+
+# BUILD DATA SET
 dat <- covid_ct %>%
-  inner_join(ct_weather)
+  filter(date >= ymd(20200401)) %>%
+  select(date, new_cases) %>%
+  left_join(ct_weather) %>%
+  select(1:3) %>%
+  filter(!is.na(tavg)) %>%
+  mutate(cases = rollapply(new_cases, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="left"),
+         temp = rollapply(tavg, width = 7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right")) %>%
+  ungroup() %>%
+  select(date, cases, temp)
+
+dat %>%
+  ggplot(aes(x = temp, y = cases)) +
+  geom_point()
+
+
+# BUILD TRAIN AND TEST SET
+y <- dat$cases
+set.seed(1, sample.kind = 'Rounding')
+test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+
+train_set <- dat %>% slice(-test_index)
+test_set <- dat %>% slice(test_index)
+
+# LOESS MODEL
+temp_range <- diff(range(train_set$temp))
+span <- 7/temp_range
+
+Ss <- seq(3:79)
+acc <- map_df(Ss, function(s){
+  span <- s / temp_range
+  fit <- loess(cases ~ temp, degree = 1, span = span, data = train_set, control=loess.control(surface="direct"))
+  y_hat <- predict(fit, test_set)
+  error <- mean((y_hat - test_set$cases)^2)
+  tibble(error = error, S = s)
+})
+
+acc %>%
+  ggplot(aes(S, error)) +
+  geom_line() +
+  geom_point()
+
+best_s <- Ss[which.min(acc$error)]
+span <- best_s / temp_range
+
+fit <- loess(cases ~ temp, degree = 1, span = span, data = train_set, control=loess.control(surface="direct"))
+fit
+
+train_set %>% 
+  mutate(smooth = fit$fitted) %>%
+  ggplot(aes(x = temp, y = cases)) +
+  geom_point(size = 1, alpha = .5, color = "grey") +
+  geom_line(aes(temp, smooth), color = "red")
+
+y_hat <- predict(fit, test_set)
+mean((y_hat - test_set$cases)^2)
+
+predict(fit, 62)
+
+
+
+
+
+
+
+
+
 
 # MAX DATE?
 max(dat$date)
