11437392516eb7d4ffd4e95860874f73daec4a30
Andrew F. Griffin <andrewfgriffin@gmail.com>
2020-11-16 16:51:10 -0500

    2020 11 15 Reporting

--- 04-state-summary.R
+++ 04-state-summary.R
@@ -10,14 +10,18 @@ options(digits = 3)
 # DAILY LOOKUP TOTAL
 covid_ct %>%
   select(date, new_cases, new_tests, percent_pos, new_deaths, current_hosp, new_hosp) %>%
-  filter(date %in% ind_tylm_date)
+  filter(date %in% ind_tylm_date) %>%
+  kable()
 
 # WEEKLY SUMS LAST 2 WEEKS
-covid_ct_sum
+covid_ct_sum %>%
+  kable()
 
 merge(covid_ct_counties_sum, covid_ct_counties_zone, by = c("county", "sum_cases_percap", "percent_pos")) %>%
   select(county, sum_cases_percap, sum_tests_percap, percent_pos, sum_deaths_percap, sum_hosp_percap, cases_zone, percent_zone) %>%
-  arrange(desc(sum_cases_percap))
+  arrange(desc(sum_cases_percap)) %>%
+  kable()
 
-town_growth_top10
+town_growth_top10 %>%
+  kable()
   
